7655898014ff0a7d63d7084e35b350818f759d41
Erik M. Buck <ebuck1@udayton.edu>
2024-09-28 13:31:58 -0400

    use stdint types or explicit bitfield length - clang-format - clang-tidy

--- buf.h
+++ buf.h
@@ -1,14 +1,21 @@
+#ifndef BF68B728_9BB0_4332_970D_840C23BD69D6
+#define BF68B728_9BB0_4332_970D_840C23BD69D6
+#include "fs.h"
+#include "sleeplock.h"
+#include "types.h"
+
 struct buf {
   int flags;
-  uint dev;
-  uint blockno;
+  uint32_t dev;
+  uint32_t blockno;
   struct sleeplock lock;
-  uint refcnt;
+  uint32_t refcnt;
   struct buf *prev; // LRU cache list
   struct buf *next;
   struct buf *qnext; // disk queue
-  uchar data[BSIZE];
+  uint8_t data[BSIZE];
 };
-#define B_VALID 0x2  // buffer has been read from disk
-#define B_DIRTY 0x4  // buffer needs to be written to disk
+#define B_VALID 0x2 // buffer has been read from disk
+#define B_DIRTY 0x4 // buffer needs to be written to disk
 
+#endif /* BF68B728_9BB0_4332_970D_840C23BD69D6 */
--- umalloc.c
+++ umalloc.c
@@ -1,7 +1,7 @@
-#include "types.h"
-#include "stat.h"
-#include "user.h"
 #include "param.h"
+#include "stat.h"
+#include "types.h"
+#include "user.h"
 
 // Memory allocator by Kernighan and Ritchie,
 // The C programming Language, 2nd ed.  Section 8.7.
@@ -11,7 +11,7 @@ typedef long Align;
 union header {
   struct {
     union header *ptr;
-    uint size;
+    uint32_t size;
   } s;
   Align x;
 };
@@ -21,70 +21,72 @@ typedef union header Header;
 static Header base;
 static Header *freep;
 
-void
-free(void *ap)
-{
+void free(void *ap) {
   Header *bp, *p;
 
-  bp = (Header*)ap - 1;
-  for(p = freep; !(bp > p && bp < p->s.ptr); p = p->s.ptr)
-    if(p >= p->s.ptr && (bp > p || bp < p->s.ptr))
+  bp = (Header *)ap - 1;
+  for (p = freep; !(bp > p && bp < p->s.ptr); p = p->s.ptr) {
+    if (p >= p->s.ptr && (bp > p || bp < p->s.ptr)) {
       break;
-  if(bp + bp->s.size == p->s.ptr){
+    }
+  }
+  if (bp + bp->s.size == p->s.ptr) {
     bp->s.size += p->s.ptr->s.size;
     bp->s.ptr = p->s.ptr->s.ptr;
-  } else
+  } else {
     bp->s.ptr = p->s.ptr;
-  if(p + p->s.size == bp){
+  }
+  if (p + p->s.size == bp) {
     p->s.size += bp->s.size;
     p->s.ptr = bp->s.ptr;
-  } else
+  } else {
     p->s.ptr = bp;
+  }
   freep = p;
 }
 
-static Header*
-morecore(uint nu)
-{
+static Header *morecore(uint32_t nu) {
   char *p;
   Header *hp;
 
-  if(nu < 4096)
+  if (nu < 4096) {
     nu = 4096;
+  }
   p = sbrk(nu * sizeof(Header));
-  if(p == (char*)-1)
+  if (p == (char *)-1) {
     return 0;
-  hp = (Header*)p;
+  }
+  hp = (Header *)p;
   hp->s.size = nu;
-  free((void*)(hp + 1));
+  free((void *)(hp + 1));
   return freep;
 }
 
-void*
-malloc(uint nbytes)
-{
+void *malloc(uint32_t nbytes) {
   Header *p, *prevp;
-  uint nunits;
+  uint32_t nunits;
 
-  nunits = (nbytes + sizeof(Header) - 1)/sizeof(Header) + 1;
-  if((prevp = freep) == 0){
+  nunits = (nbytes + sizeof(Header) - 1) / sizeof(Header) + 1;
+  if ((prevp = freep) == 0) {
     base.s.ptr = freep = prevp = &base;
     base.s.size = 0;
   }
-  for(p = prevp->s.ptr; ; prevp = p, p = p->s.ptr){
-    if(p->s.size >= nunits){
-      if(p->s.size == nunits)
+  for (p = prevp->s.ptr;; prevp = p, p = p->s.ptr) {
+    if (p->s.size >= nunits) {
+      if (p->s.size == nunits) {
         prevp->s.ptr = p->s.ptr;
-      else {
+      } else {
         p->s.size -= nunits;
         p += p->s.size;
         p->s.size = nunits;
       }
       freep = prevp;
-      return (void*)(p + 1);
+      return (void *)(p + 1);
     }
-    if(p == freep)
-      if((p = morecore(nunits)) == 0)
+    if (p == freep) {
+      if ((p = morecore(nunits)) == 0) {
         return 0;
+      }
+    }
   }
 }
